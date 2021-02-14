40dd98c196d5929ad6e605e196225946cb58bc49
jonki324 <jonki324@gmail.com>
2021-02-15 02:13:52 +0900

    JPA追加

---
+++ src/main/java/com/example/TodoDao.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@RequestScoped
+public class TodoDao {
+	@PersistenceContext(name = "jpa-unit")
+	private EntityManager em;
+	
+	public void create(TodoDto todo) {
+		em.persist(todo);
+	}
+	
+	public TodoDto read(int id) {
+		return em.find(TodoDto.class, id);
+	}
+	
+	public void update(TodoDto todo) {
+		em.merge(todo);
+	}
+	
+	public void delete(TodoDto todo) {
+		em.remove(todo);
+	}
+}
--- src/main/java/com/example/TodoDto.java
+++ src/main/java/com/example/TodoDto.java
@@ -1,5 +1,12 @@
 package com.example;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +14,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "todos")
 public class TodoDto {
-  private Integer id;
-  private String task;
-  private Boolean isDone;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "task")
+	private String task;
+	
+	@Column(name = "isDone")
+	private Boolean isDone;
 }
